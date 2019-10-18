8dfe909ec1b15c18cbe9971fd7b2e582878fd9a0
Filipe Merli <filipemerli@air-de-filipe.home>
2019-10-18 08:38:53 -0300

    Added stars count

--- GitHub SwiftByStarts/Model/Repositorie.swift
+++ GitHub SwiftByStarts/Model/Repositorie.swift
@@ -11,17 +11,20 @@ struct Repositorie {
     let owner: UserBio
     let name: String
     let language: String?
+    let stars: Int?
     
     enum CodingKeys: String, CodingKey {
         case owner
         case name
         case language
+        case stars = "stargazers_count"
     }
     
-    init(owner: UserBio, name: String, language: String) {
+    init(owner: UserBio, name: String, language: String, stars: Int) {
         self.owner = owner
         self.name = name
         self.language = language
+        self.stars = stars
     }
 }
 
@@ -31,7 +34,8 @@ extension Repositorie: Decodable {
         let owner = try container.decode(UserBio.self, forKey: .owner)
         let name = try container.decode(String.self, forKey: .name)
         let language = (try? container.decode(String.self, forKey: .language)) ?? ""
-        self.init(owner: owner, name: name, language: language)
+        let stars = (try? container.decode(Int.self, forKey: .stars)) ?? 0
+        self.init(owner: owner, name: name, language: language, stars: stars)
     }
     
 }
--- GitHub SwiftByStarts/Views/TableViewsComponents/RepositoriesByStarsCell.swift
+++ GitHub SwiftByStarts/Views/TableViewsComponents/RepositoriesByStarsCell.swift
@@ -25,8 +25,14 @@ class RepositoriesByStarsCell: UITableViewCell {
         image.translatesAutoresizingMaskIntoConstraints = false
         image.layer.cornerRadius = 35
         image.clipsToBounds = true
-        image.backgroundColor = .lightGray
         return image
+    }()
+    
+    let activityIndicator: UIActivityIndicatorView = {
+        let activity = UIActivityIndicatorView()
+        activity.hidesWhenStopped = true
+        activity.translatesAutoresizingMaskIntoConstraints = false
+        return activity
     }()
     
     let repoLabel: UILabel = {
@@ -44,22 +50,34 @@ class RepositoriesByStarsCell: UITableViewCell {
         return label
     }()
     
+    let starsLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.clipsToBounds = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
 
     // MARK: Class Functions
     
     func setCell(with repo: Repositorie?) {
-        
+        activityIndicator.startAnimating()
         if let repo = repo {
             repoLabel.text = "Repo: \(repo.name)"
+            let stars = repo.stars!
             ownerNameLabel.text = "Owner: \(repo.owner.login)"
+            starsLabel.text = "Stars: \(stars)"
             profileImageView.loadImageWithUrl(theUrl: repo.owner.avatarUrl) { result in
                 switch result {
                 case .failure ( _):
                     DispatchQueue.main.async {
+                        self.activityIndicator.stopAnimating()
                         self.profileImageView.image = #imageLiteral(resourceName: "person_placeholder")
                     }
                 case .success(let response):
                     DispatchQueue.main.async {
+                        self.activityIndicator.stopAnimating()
                         self.profileImageView.image = response.profileImage
                     }
                 }
@@ -67,14 +85,16 @@ class RepositoriesByStarsCell: UITableViewCell {
         }
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        profileImageView.image = #imageLiteral(resourceName: "person_placeholder")
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        setCell(with: .none)
+        activityIndicator.startAnimating()
     }
     
-    override func prepareForReuse() {
-      super.prepareForReuse()
-      setCell(with: .none)
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        activityIndicator.startAnimating()
     }
     
     // MARK: Initializers
@@ -82,29 +102,38 @@ class RepositoriesByStarsCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        self.contentView.addSubview(profileImageView)
+        contentView.addSubview(profileImageView)
+        contentView.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
         containerView.addSubview(repoLabel)
         containerView.addSubview(ownerNameLabel)
-        self.contentView.addSubview(containerView)
+        containerView.addSubview(starsLabel)
+        contentView.addSubview(containerView)
     
-        profileImageView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
-        profileImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant:10).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        profileImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant:10).isActive = true
         profileImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
         profileImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
         
-        containerView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
-        containerView.leadingAnchor.constraint(equalTo: self.profileImageView.trailingAnchor, constant: 10).isActive = true
-        containerView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -10).isActive = true
-        containerView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        activityIndicator.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        activityIndicator.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant:30).isActive = true
+        activityIndicator.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        activityIndicator.heightAnchor.constraint(equalToConstant: 30).isActive = true
         
-        repoLabel.topAnchor.constraint(equalTo: self.containerView.topAnchor).isActive = true
-        repoLabel.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor).isActive = true
-        repoLabel.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor).isActive = true
+        containerView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        containerView.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 10).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 60).isActive = true
         
-        ownerNameLabel.topAnchor.constraint(equalTo: self.repoLabel.bottomAnchor).isActive = true
-        ownerNameLabel.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor).isActive = true
-        ownerNameLabel.topAnchor.constraint(equalTo: self.repoLabel.bottomAnchor).isActive = true
-        ownerNameLabel.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor).isActive = true
+        repoLabel.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        repoLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        repoLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+        
+        ownerNameLabel.topAnchor.constraint(equalTo: repoLabel.bottomAnchor).isActive = true
+        ownerNameLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        
+        starsLabel.topAnchor.constraint(equalTo: ownerNameLabel.bottomAnchor).isActive = true
+        starsLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
         
     }
     
